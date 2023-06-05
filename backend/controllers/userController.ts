99d124d5e8a194286cbb8e4a5839fde3b5c8f5ed
Sapien <sidjha0001@gmail.com>
2023-06-05 00:16:43 -0600

    added authmiddleware and jwt

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
 import pool from "../backend";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
     try {
 
+        const { name, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const query = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`;
 
-        const values = [req.body.name, req.body.email, req.body.password];
+        const values = [name, email, hashedPassword];
 
         const { rows } = await pool.query(query, values);
 
@@ -22,7 +28,30 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
     try {
-        res.status(200).json({ message: "User logged in sucessfully" });
+
+        const { email, password } = req.body;
+
+        // Check if the user exists in the database
+        const query = `SELECT * FROM users WHERE email = $1`;
+        const result = await pool.query(query, [email]);
+        const user = result.rows[0];
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Compare the provided password with the hashed password in the database
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // Generate and sign a JWT token
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+
+        res.status(200).json({ user, token });
     }
     catch (err) {
         res.status(500).json({ message: 'An error occured' });
--- backend/middlewares/authMiddleware.ts
+++ backend/middlewares/authMiddleware.ts
@@ -1,14 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+interface CustomRequest extends Request {
+    user?: any;
+}
+
+const authMiddleware = (req: CustomRequest, res: Response, next: NextFunction) => {
     try {
-        const isAuth = true;
-        if (isAuth) {
-            next();
-        }
-        else {
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
             res.status(401).json({ message: "You are not authorized" });
         }
+
+        const decodedToken = jwt.verify(token!, process.env.JWT_SECRET!);
+
+        req.user = decodedToken;
+
+        next();
     } catch (err) {
         res.status(500).json({ message: 'An error occured' });
     }
